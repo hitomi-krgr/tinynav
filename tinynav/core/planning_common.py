05022637b8605479d4605cbbfc75d921a0ee6605
hitomi-krgr <jiawei.chen@deepmirror.com>
2026-06-24 13:55:54 +0800

    fix(planning_common): update width value for B2_CONFIG from 0.05 to 0.1 fix(cmd_vel_control): increase maximum velocity limit from 0.3 to 0.4

--- tinynav/core/planning_common.py
+++ tinynav/core/planning_common.py
@@ -68,7 +68,7 @@ GO2_CONFIG = RobotConfig(
 
 B2_CONFIG = RobotConfig(
     name='b2', shape='square',
-    length=0.8, width=0.05,
+    length=0.8, width=0.1,
     camera_x=0.5, camera_y=0.0,
     control_x=0.0, control_y=0.0,
     safety_radius=0.2,
@@ -217,7 +217,7 @@ def generate_trajectory_library_3d(
     """Regular sampled lattice (forward-only)."""
     num_steps = int(duration / dt) + 1
 
-    vx_max = 0.3
+    vx_max = 0.4
     n_vx = max(3, int(num_samples / 2))
     vx_samples = np.linspace(0.0, vx_max, n_vx)
     omega_y_samples = np.linspace(-np.pi / 3, np.pi / 3, num_samples)
