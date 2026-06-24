8a59323b285e9cc32fa555973a6cfbc40f787d43
hitomi-krgr <jiawei.chen@deepmirror.com>
2026-06-24 10:19:29 +0800

    fix(cmd_vel_control): update max angular speed to align with new planner omega range fix(planning_node_turn): set K_TURN_CLEAR to zero for improved trajectory clearance

--- tinynav/core/planning_common.py
+++ tinynav/core/planning_common.py
@@ -220,7 +220,7 @@ def generate_trajectory_library_3d(
     vx_max = 0.5
     n_vx = max(3, int(num_samples / 2))
     vx_samples = np.linspace(0.0, vx_max, n_vx)
-    omega_y_samples = np.linspace(-np.pi / 2, np.pi / 2, num_samples)
+    omega_y_samples = np.linspace(-np.pi / 3, np.pi / 3, num_samples)
 
     num_samples = len(vx_samples) * len(omega_y_samples)
 
